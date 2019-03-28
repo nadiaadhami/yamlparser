5c72a147dd55d5e5b260906b38727dbbae84a8cd
fpb873 <nadia.adhami@hallmark.com>
2019-03-28 15:44:55 -0700

    states

---
+++ models/states.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type States struct {
+	ID           int64  `orm:"column(id);pk;"`
+	Name         string `orm:"column(name);size(255)"`
+	Code         string `orm:"column(code);size(2)"`
+	CountryAlpha string `orm:"column(country_alpha);size(2)"`
+
+	InsertedAt time.Time `orm:"column(inserted_at);auto_now_add;type(datetime)"`
+	UpdatedAt  time.Time `orm:"column(updated_at);auto_now_add;type(datetime)"`
+}
+
+func (p *States) TableName() string {
+	return "states"
+}
