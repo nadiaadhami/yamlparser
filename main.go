d8fd1c24734754d504ecf7189dc692909db12c77
fpb873 <nadia.adhami@hallmark.com>
2019-03-29 10:03:53 -0700

    fmt files

--- main.go
+++ main.go
@@ -79,7 +79,7 @@ func main() {
 	subs := database.LoadSubdivisions(d)
 	fmt.Println("\nTotal subdivision =", len(subs))
 
-	stateMap := database.GetStates("US","./database/data/subdivisions/US.yaml")
+	stateMap := database.GetStates("US", "./database/data/subdivisions/US.yaml")
 	database.PrintStates(stateMap)
 }
 
